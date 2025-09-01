ea6974dd19571ae9cacdb5e4e79e0521273a04e9
Burhan Ahmed <burhan.ahmed@advantal.net>
2025-09-01 11:21:43 +0530

    add the interval and change the message and also add the cancel button.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [moviesList, setMoviesList] = useState([]);
-  const [isLoading , setIsloading] = useState(false);
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
+
   const fetchMoviesList = async () => {
     try {
       setIsloading(true);
-      const response = await fetch("https://swapi.py4e.com/api/films/");      
+      const response = await fetch("https://swapi.py4e.com/api/film/");
+      if(!response.ok){
+        throw new Error("Something went wrong.");
+      }
       const movies = await response.json()
       console.log(movies.results)
       setMoviesList(movies.results)
       setIsloading(false);
     } catch (err) {
+      setError(err)
       setIsloading(false);
       console.log(err)
     }
+  };
 
-  }
+  useEffect(() => {
+    let fetchInterval      
+    if (error) {
+      console.log("in interval ")
+      fetchInterval = setInterval(() => {
+        fetchMoviesList();
+      }, 5000);
+      
+    }else{
+      clearInterval(fetchInterval);
+    }
+    return ()=> clearInterval(fetchInterval)
+  }, [error])
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesList}>Fetch Movies</button>
+        {error ? <button onClick={()=>setError(null)}>Cancel</button>: <button onClick={fetchMoviesList}>Fetch Movies</button> }
+        
       </section>
       <section>
-        {isLoading ? <div>Loading...</div>:
-        <MoviesList movies={moviesList} />}
+        {isLoading ? <div>Loading...</div> :
+          error ? <p>Something went wrong ....Retrying</p> :
+            moviesList.length === 0 ? <p>Found no Movies</p> : <MoviesList movies={moviesList} />}
       </section>
     </React.Fragment>
   );
